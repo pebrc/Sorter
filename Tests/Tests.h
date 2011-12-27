5906524f9888ce6760ed2b6f2f04e298d54e019d
Peter Brachwitz <peter.brachwitz@gmail.com>
2011-12-27 12:48:55 +0100

    removed GHUnit and went back to OCUnit

---
+++ Tests/Tests.h
@@ -0,0 +1,13 @@
+//
+//  Tests.h
+//  Tests
+//
+//  Created by Peter Brachwitz on 27/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tests : SenTestCase
+
+@end
---
+++ Tests/Tests.m
@@ -0,0 +1,32 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Peter Brachwitz on 27/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Tests.h"
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Tests");
+}
+
+@end
