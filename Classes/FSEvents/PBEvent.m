3d886f89436db642b4a7416ab306f132e9b7ae7e
Peter Brachwitz <peter.brachwitz@gmail.com>
2011-12-28 22:13:02 +0100

    ack stu connolloy

--- Classes/FSEvents/PBEvent.m
+++ Classes/FSEvents/PBEvent.m
@@ -1,4 +1,5 @@
 //Copyright (c) 2011 Peter Brachwitz
+// With some inspiration from SCEvents Copyright (c) 2009 Stuart Connolly
 //
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
