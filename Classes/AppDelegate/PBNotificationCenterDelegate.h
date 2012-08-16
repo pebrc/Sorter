a6745ccc08bb444b1e9277d99430d08e1971246e
Peter Brachwitz <peter.brachwitz@gmail.com>
2012-08-16 14:03:43 +0200

    Added generic user notification system and support for new 10.8 notification center
    Instead of logging and growling seperately it seemed reasonable to unify
    both activities as two diffrent forms of the same thing in one class.
    The new class allows plugging in arbitrary notification delegates to
    serve different notification systems like growl, a logger or the
    built-in NSUserNotificationCenter.

---
+++ Classes/AppDelegate/PBNotificationCenterDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PBNotificationCenterDelegate.h
+//  Sorter
+//
+//  Created by Peter Brachwitz on 16/08/2012.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "PBUserNotify.h"
+
+@interface PBNotificationCenterDelegate : NSObject <PBNotifyDelegate>
+
+
+@end
